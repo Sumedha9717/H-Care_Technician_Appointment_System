65af0979ceee9ffdf4bcc487af77cc63cf14ef44
Sumedha Bandara <sumnew2i@gmail.com>
2025-09-12 21:19:19 +0530

    fix cancel button

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -205,4 +205,39 @@ const listAppointment = async (req,res) => {
     }
 }
 
-export {registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment}
+// API to cancel appointment
+const cancelAppointment = async (req,res) => {
+    try {
+
+         const userId = req.userId
+         const {appointmentId} = req.body
+
+         const appointmentData = await appointmentModel.findById(appointmentId)
+
+        //  verify appointment user
+        if (appointmentData.userId !== userId) {
+            return res.json({success:false,message:'Unauthorized action'})
+        }
+
+        await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled:true})
+
+        // releasing doctor slot
+        const {tecId, slotDate, slotTime} = appointmentData
+
+        const technicianData = await technicianModel.findById(tecId)
+
+        let slots_booked = technicianData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime)
+
+        await technicianModel.findByIdAndUpdate(tecId, {slots_booked})
+
+        res.json({success:true,message:'Appointment Cancelled'})
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export {registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment}
--- h_care_system/src/pages/MyAppointments.jsx
+++ h_care_system/src/pages/MyAppointments.jsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 
 const MyAppointments = () => {
 
-const { backendUrl, token } = useContext(AppContext)
+const { backendUrl, token, getTechniciansData } = useContext(AppContext)
 
 const [appointments,setAppointments] = useState([])
 const months = ["","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
@@ -25,6 +25,24 @@ const getUserAppointments = async () => {
         if (data.success) {
             setAppointments(data.appointments?.reverse() || [])
             console.log(data.appointments);
+        }
+        
+    } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+    }
+}
+
+const cancelAppointment = async (appointmentId) => {
+    try {
+
+        const {data} = await axios.post(backendUrl + '/api/user/cancel-appointment', {appointmentId},{headers:{token}})
+        if (data.success) {
+            toast.success(data.message)
+            getUserAppointments()
+            getTechniciansData()
+        } else {
+            toast.error(data.message)
         }
         
     } catch (error) {
@@ -58,8 +76,9 @@ useEffect(()=>{
                     </div> 
                     <div></div>
                     <div className='flex flex-col gap-2 justify-end'>
-                        <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-green-600 hover:text-white transition-all duration-300'>Pay</button>
-                        <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
+                        {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-green-600 hover:text-white transition-all duration-300'>Pay</button>}
+                        {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+                        {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-600 '>Appointment cancelled</button>}
                     </div>
                 </div>
             ))}
